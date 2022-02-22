a2ea2e3b1cfd37454f21e223e83f0eead786ece3
Victor Mateus <victormateus2003@gmail.com>
2022-02-22 16:51:25 -0300

    Simple_Calculator

--- dynamic.js
+++ dynamic.js
@@ -1,9 +1,22 @@
-// simple function to sum the numbers
-function myFunction() {
-    
-    var first_number = Number(document.getElementById("fnumber").value)
-    var last_number = Number(document.getElementById("snumber").value)
-    
-    var sum = first_number + last_number
-    document.getElementById("sum").innerHTML = sum
+// variables with the values
+var first_number = Number(document.getElementById("fnumber").value)
+var last_number = Number(document.getElementById("snumber").value)
+
+
+// each function does a different operation
+function sumFunction(fnumber, snumber) { 
+    let sum = fnumber + snumber
+    document.getElementById("result").innerHTML = sum
+}
+function subFunction(fnumber, snumber) {
+    let sub = fnumber - snumber
+    document.getElementById("result").innerHTML = sub
+}
+function prodFunction(fnumber, snumber) {
+    let prod = fnumber * snumber
+    document.getElementById("result").innerHTML = prod
+}
+function divFunction(fnumber, snumber) {
+    let div = fnumber / snumber
+    document.getElementById("result").innerHTML = div
 }

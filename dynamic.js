f9cd004f4eb0fe12b3b2634a6782878b10eb7617
Victor Mateus <98140122+VictorrMatt@users.noreply.github.com>
2022-02-22 18:15:10 -0300

    Add files via upload
    code optimized

--- dynamic.js
+++ dynamic.js
@@ -1,9 +1,5 @@
 // simple function to sum the numbers
-function myFunction() {
-    
-    var first_number = Number(document.getElementById("fnumber").value)
-    var last_number = Number(document.getElementById("snumber").value)
-    
-    var sum = first_number + last_number
-    document.getElementById("sum").innerHTML = sum
+function sumFunction(fnumber, snumber) {
+    let sum = fnumber + snumber
+    document.getElementById("result").innerHTML = sum
 }
